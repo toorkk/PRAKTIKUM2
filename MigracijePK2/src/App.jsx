b00dc7451b9b9a6e183775ee5a5526ffb54de6af
vaskolinko <vasjarimele123@gmail.com>
2024-05-17 12:31:31 +0200

    stimajo zaj ikonice pa mapica se lepo prilagaja!

--- MigracijePK2/src/App.jsx
+++ MigracijePK2/src/App.jsx
@@ -3,14 +3,13 @@ import MapComponent from './components/MapComponent';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Sidebar from './components/SideBar.jsx';
 
+
 function App() {
   return (
     <>
       <Router>
         <div className="App">
-          <div className="Sidebar">
             <Sidebar />
-          </div>
           <div className="MapComponent">
             <MapComponent />
           </div>
