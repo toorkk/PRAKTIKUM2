c8ef6081fc721bcdc723df93129fddddbd12ab29
AnzeHa <torkermeco@gmail.com>
2024-05-17 12:41:27 +0200

    woah

--- MigracijePK2/src/components/MapComponent.jsx
+++ MigracijePK2/src/components/MapComponent.jsx
@@ -1,17 +1,20 @@
 import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet'
-import OBCINA from '../../data/OBCINE.json'
+import ObcineGeo from '../../data/OBCINE.json'
+import RegijeGeo from '../../data/SR.json'
 
 function MapComponent() {
 
   return (
     <>
 
-      <MapContainer id='map' center={[46.07118, 15.0]} zoom={8} scrollWheelZoom={true}>
+      <MapContainer id='map' center={[46.07118, 15.6]} zoom={8} scrollWheelZoom={true} placeholder={<h1>MAPA SE NALAGA, MOGOČE MORATE OMOGOČITI JAVASCRIPT</h1>}>
+        
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <GeoJSON data={OBCINA} />
+
+        <GeoJSON data={ObcineGeo} attribution="&copy; Štefan Baebler"/>
 
       </MapContainer>
 
