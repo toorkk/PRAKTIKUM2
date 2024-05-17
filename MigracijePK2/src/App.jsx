3112dc0e586ad9c60c33017f9307efdf81983379
AnzeHa <torkermeco@gmail.com>
2024-05-17 10:40:49 +0200

    za vasja <3

--- MigracijePK2/src/App.jsx
+++ MigracijePK2/src/App.jsx
@@ -1,28 +1,15 @@
-import { useState } from 'react'
 import './App.css'
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import MapComponent from './components/MapComponent'
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
 
       <h1>MIGRACIJE</h1>
-      <MapContainer id='map' center={[46.4131077, 15.8648681]} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[46.4131077, 15.8648681]}>
-          <Popup>
-            VEM KJE ŽIVIŠ
-          </Popup>
-        </Marker>
-      </MapContainer>
-
+      <MapComponent/>
 
     </>
   )
---
+++ MigracijePK2/src/components/MapComponent.jsx
@@ -0,0 +1,28 @@
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+
+
+
+function MapComponent() {
+
+  return (
+    <>
+
+      <h1>MIGRACIJE</h1>
+      <MapContainer id='map' center={[46.4131077, 15.8648681]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[46.4131077, 15.8648681]}>
+          <Popup>
+            VEM KJE ŽIVIŠ
+          </Popup>
+        </Marker>
+      </MapContainer>
+
+
+    </>
+  )
+}
+
+export default MapComponent;
