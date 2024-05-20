253a2390296d5c28b27af11117b6bbb3916c2aa2
AnzeHa <torkermeco@gmail.com>
2024-05-20 09:53:37 +0200

    dodal spreminjane prikaza med regijami in občinami

--- MigracijePK2/src/components/MapComponent.jsx
+++ MigracijePK2/src/components/MapComponent.jsx
@@ -1,8 +1,27 @@
-import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet'
+import { useState, useEffect } from 'react'
+import { MapContainer, TileLayer, GeoJSON, useMapEvent } from 'react-leaflet'
 import ObcineGeo from '../../data/OBCINE.json'
 import RegijeGeo from '../../data/SR.json'
 
+
 function MapComponent() {
+
+  const [layer, setLayer] = useState('Regije');
+
+  function MapInnard() {
+    const map = useMapEvent('zoomend', () => {
+      if(map.getZoom() > 9){
+        setLayer('Obcine');
+      }
+      else{   
+        setLayer('Regije');
+      }
+    })
+  }
+
+  useEffect(() => {
+    console.log('Current layer:', layer);
+  }, [layer]);
 
   return (
     <>
@@ -13,8 +32,14 @@ function MapComponent() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-
-        <GeoJSON data={ObcineGeo} attribution="&copy; Štefan Baebler"/>
+        
+        <MapInnard setLayer={setLayer} />
+        {layer === 'Obcine' && (
+          <GeoJSON data={ObcineGeo} attribution="&copy; Štefan Baebler" />
+        )}
+        {layer === 'Regije' && (
+          <GeoJSON data={RegijeGeo} attribution="&copy; Štefan Baebler" />
+        )}
 
       </MapContainer>
 
