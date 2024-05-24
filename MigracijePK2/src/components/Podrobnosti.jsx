c456291b29058de8aac160419e834f8af91be871
vaskolinko <vasjarimele123@gmail.com>
2024-05-24 10:35:07 +0200

    Porihto se malo navigacijo med stranema

---
+++ MigracijePK2/src/components/Podrobnosti.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Podrobnosti = () => {
+  return (
+    <div>
+      <h1>Podrobnosti Component</h1>
+      {/* Add your detailed content here */}
+    </div>
+  );
+}
+
+export default Podrobnosti;
